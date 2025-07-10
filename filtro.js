65d5551f9af9b57b6ee2a0b1ae8aa5bc2a96f049
Esraa <esraasamy03@gmail.com>
2025-07-10 12:03:06 +0200

    Filtro

--- filtro.js
+++ filtro.js
@@ -1,9 +1,172 @@
 const ingredients = {
-"meals":[
-    {"idIngredient":"1","strIngredient":"Chicken","strDescription":"The chicken is a type of domesticated fowl, a subspecies of the red junglefowl (Gallus gallus). It is one of the most common and widespread domestic animals, with a total population of more than 19 billion as of 2011. There are more chickens in the world than any other bird or domesticated fowl. Humans keep chickens primarily as a source of food (consuming both their meat and eggs) and, less commonly, as pets. Originally raised for cockfighting or for special ceremonies, chickens were not kept for food until the Hellenistic period (4th\u20132nd centuries BC).\r\n\r\nGenetic studies have pointed to multiple maternal origins in South Asia, Southeast Asia, and East Asia, but with the clade found in the Americas, Europe, the Middle East and Africa originating in the Indian subcontinent. From ancient India, the domesticated chicken spread to Lydia in western Asia Minor, and to Greece by the 5th century BC. Fowl had been known in Egypt since the mid-15th century BC, with the \"bird that gives birth every day\" having come to Egypt from the land between Syria and Shinar, Babylonia, according to the annals of Thutmose III.","strType":null},
-    {"idIngredient":"2","strIngredient":"Salmon","strDescription":"Salmon is the common name for several species of ray-finned fish in the family Salmonidae. Other fish in the same family include trout, char, grayling and whitefish. Salmon are native to tributaries of the North Atlantic (genus Salmo) and Pacific Ocean (genus Oncorhynchus). Many species of salmon have been introduced into non-native environments such as the Great Lakes of North America and Patagonia in South America. Salmon are intensively farmed in many parts of the world.\r\n\r\nTypically, salmon are anadromous: they hatch in fresh water, migrate to the ocean, then return to fresh water to reproduce. However, populations of several species are restricted to fresh water through their lives. Folklore has it that the fish return to the exact spot where they hatched to spawn. Tracking studies have shown this to be mostly true. A portion of a returning salmon run may stray and spawn in different freshwater systems; the percent of straying depends on the species of salmon. Homing behavior has been shown to depend on olfactory memory. Salmon date back to the Neogene.","strType":null},
-]
+  "meals": [
+    {
+      "idIngredient": "1",
+      "strIngredient": "Chicken",
+      "strDescription": "The chicken is a type of domesticated fowl, a subspecies of the red junglefowl (Gallus gallus). It is one of the most common and widespread domestic animals, with a total population of more than 19 billion as of 2011. There are more chickens in the world than any other bird or domesticated fowl. Humans keep chickens primarily as a source of food (consuming both their meat and eggs) and, less commonly, as pets. Originally raised for cockfighting or for special ceremonies, chickens were not kept for food until the Hellenistic period (4th–2nd centuries BC).\r\n\r\nGenetic studies have pointed to multiple maternal origins in South Asia, Southeast Asia, and East Asia, but with the clade found in the Americas, Europe, the Middle East and Africa originating in the Indian subcontinent. From ancient India, the domesticated chicken spread to Lydia in western Asia Minor, and to Greece by the 5th century BC. Fowl had been known in Egypt since the mid-15th century BC, with the \"bird that gives birth every day\" having come to Egypt from the land between Syria and Shinar, Babylonia, according to the annals of Thutmose III.",
+      "strType": null
+    },
+    {
+      "idIngredient": "2",
+      "strIngredient": "Salmon",
+      "strDescription": "Salmon is the common name for several species of ray-finned fish in the family Salmonidae. Other fish in the same family include trout, char, grayling and whitefish. Salmon are native to tributaries of the North Atlantic (genus Salmo) and Pacific Ocean (genus Oncorhynchus). Many species of salmon have been introduced into non-native environments such as the Great Lakes of North America and Patagonia in South America. Salmon are intensively farmed in many parts of the world.\r\n\r\nTypically, salmon are anadromous: they hatch in fresh water, migrate to the ocean, then return to fresh water to reproduce. However, populations of several species are restricted to fresh water through their lives. Folklore has it that the fish return to the exact spot where they hatched to spawn. Tracking studies have shown this to be mostly true. A portion of a returning salmon run may stray and spawn in different freshwater systems; the percent of straying depends on the species of salmon. Homing behavior has been shown to depend on olfactory memory. Salmon date back to the Neogene.",
+      "strType": null
+    },
+    {
+      "idIngredient": "3",
+      "strIngredient": "Beef",
+      "strDescription": "Beef is the culinary name for meat from cattle, particularly skeletal muscle. Humans have been eating beef since prehistoric times. Beef is a source of high-quality protein and nutrients.\r\n\r\nMost beef skeletal muscle meat can be used as is by merely cutting into certain parts, such as roasts, short ribs or steak (filet mignon, sirloin steak, rump steak, rib steak, rib eye steak, hanger steak, etc.), while other cuts are processed (corned beef or beef jerky). Trimmings, on the other hand, are usually mixed with meat from older, leaner (therefore tougher) cattle, are ground, minced or used in sausages. The blood is used in some varieties called blood sausage. Other parts that are eaten include other muscles and offal, such as the oxtail, liver, tongue, tripe from the reticulum or rumen, glands (particularly the pancreas and thymus, referred to as sweetbread), the heart, the brain (although forbidden where there is a danger of bovine spongiform encephalopathy, BSE, commonly referred to as mad cow disease), the kidneys, and the tender testicles of the bull (known in the United States as calf fries, prairie oysters, or Rocky Mountain oysters). Some intestines are cooked and eaten as is, but are more often cleaned and used as natural sausage casings. The bones are used for making beef stock.",
+      "strType": null
+    },
+    {
+      "idIngredient": "5",
+      "strIngredient": "Avocado",
+      "strDescription": "The avocado, a tree with probable origin in South Central Mexico, is classified as a member of the flowering plant family Lauraceae. The fruit of the plant, also called an avocado (or avocado pear or alligator pear), is botanically a large berry containing a single large seed.\r\n\r\nAvocados are commercially valuable and are cultivated in tropical and Mediterranean climates throughout the world. They have a green-skinned, fleshy body that may be pear-shaped, egg-shaped, or spherical. Commercially, they ripen after harvesting. Avocado trees are partially self-pollinating, and are often propagated through grafting to maintain predictable fruit quality and quantity. In 2017, Mexico produced 34% of the world supply of avocados.",
+      "strType": null
+    },
+    {
+      "idIngredient": "10",
+      "strIngredient": "Asparagus",
+      "strDescription": "Asparagus, or garden asparagus, folk name sparrow grass, scientific name Asparagus officinalis, is a perennial flowering plant species in the genus Asparagus. Its young shoots are used as a spring vegetable.\r\n\r\nIt was once classified in the lily family, like the related Allium species, onions and garlic. However, genetic research places lilies, Allium, and asparagus in three separate families—the Liliaceae, Amaryllidaceae, and Asparagaceae, respectively—with the Amaryllidaceae and Asparagaceae being grouped together in the order Asparagales. Sources differ as to the native range of Asparagus officinalis, but generally include most of Europe and western temperate Asia. It is widely cultivated as a vegetable crop.",
+      "strType": null
+    },
+    {
+      "idIngredient": "19",
+      "strIngredient": "Basmati Rice",
+      "strDescription": "Basmati is a variety of long, slender-grained aromatic rice which is traditionally from the Indian subcontinent. As of 2018-19, India exported to over 90% of the overseas basmati rice market, while Pakistan accounted for the remainder, according to the Indian state-run Agricultural and Processed Food Products Export Development Authority  and the Pakistan government-run Economic Survey of Pakistan. Many countries use domestically grown basmati rice crops; however, basmati is geographically exclusive to select districts of India, Pakistan, Nepal and Bangladesh.",
+      "strType": null
+    },
+    {
+      "idIngredient": "29",
+      "strIngredient": "Black Pepper",
+      "strDescription": "Black pepper (Piper nigrum) is a flowering vine in the family Piperaceae, cultivated for its fruit, known as a peppercorn, which is usually dried and used as a spice and seasoning. When fresh and fully mature, it is about 5 mm (0.20 in) in diameter and dark red, and contains a single seed, like all drupes. Peppercorns and the ground pepper derived from them may be described simply as pepper, or more precisely as black pepper (cooked and dried unripe fruit), green pepper (dried unripe fruit), or white pepper (ripe fruit seeds).\r\n\r\nBlack pepper is native to present-day Kerala in Southwestern India, and is extensively cultivated there and elsewhere in tropical regions. Vietnam is the world's largest producer and exporter of pepper, producing 34% of the world's crop, as of 2013.",
+      "strType": null
+    },
+  ]
 }
+
+const category = {
+
+  "meals": [
+    {
+      "strCategory": "Breakfast"
+    },
+    {
+      "strCategory": "Dessert"
+    },
+    {
+      "strCategory": "Seafood"
+    },
+    {
+      "strCategory": "Side"
+    },
+    {
+      "strCategory": "Starter"
+    },
+    {
+      "strCategory": "Vegan"
+    },
+    {
+      "strCategory": "Vegetarian"
+    }
+  ]
+
+}
+
+const country = {
+
+  "meals": [
+    {
+      "strArea": "American"
+    },
+    {
+      "strArea": "British"
+    },
+    {
+      "strArea": "Canadian"
+    },
+    {
+      "strArea": "Chinese"
+    },
+    {
+      "strArea": "Croatian"
+    },
+    {
+      "strArea": "Dutch"
+    },
+    {
+      "strArea": "Egyptian"
+    },
+    {
+      "strArea": "Filipino"
+    },
+    {
+      "strArea": "French"
+    },
+    {
+      "strArea": "Greek"
+    },
+    {
+      "strArea": "Indian"
+    },
+    {
+      "strArea": "Irish"
+    },
+    {
+      "strArea": "Italian"
+    },
+    {
+      "strArea": "Jamaican"
+    },
+    {
+      "strArea": "Japanese"
+    },
+    {
+      "strArea": "Kenyan"
+    },
+    {
+      "strArea": "Malaysian"
+    },
+    {
+      "strArea": "Mexican"
+    },
+    {
+      "strArea": "Moroccan"
+    },
+    {
+      "strArea": "Polish"
+    },
+    {
+      "strArea": "Portuguese"
+    },
+    {
+      "strArea": "Russian"
+    },
+    {
+      "strArea": "Spanish"
+    },
+    {
+      "strArea": "Thai"
+    },
+    {
+      "strArea": "Tunisian"
+    },
+    {
+      "strArea": "Turkish"
+    },
+    {
+      "strArea": "Ukrainian"
+    },
+    {
+      "strArea": "Uruguayan"
+    },
+    {
+      "strArea": "Vietnamese"
+    }
+  ]
+
+}
+
 
 // DOM Elements
 const categorySelect = document.getElementById('category');
@@ -12,13 +175,37 @@ const countrySelect = document.getElementById('country');
 const searchBtn = document.getElementById('search-btn');
 const resultsContainer = document.getElementById('results-container');
 
-
+// Filter by ingredient
 const ingredientSelect1 = document.getElementById("ingredient");
 
-for (let i=0; i<ingredients.meals.length; i++){
-const meal = ingredients.meals[i]
+for (let i = 0; i < ingredients.meals.length; i++) {
+  const meal = ingredients.meals[i]
   const option = document.createElement("option");
   option.setAttribute("value", meal.strIngredient);
   option.textContent = meal.strIngredient;
   ingredientSelect1.appendChild(option);
 }
+
+
+// Filter by Category
+const categorySelect1 = document.getElementById("category");
+
+for (let i = 0; i < category.meals.length; i++) {
+  const meal = category.meals[i]
+  const option = document.createElement("option");
+  option.setAttribute("value", meal.strCategory);
+  option.textContent = meal.strCategory;
+  categorySelect1.appendChild(option);
+}
+
+// Filter by country
+const countrySelect1 = document.getElementById("country");
+
+for (let i = 0; i < country.meals.length; i++) {
+  const meal = country.meals[i]
+  const option = document.createElement("option");
+  option.setAttribute("value", meal.strArea);
+  option.textContent = meal.strArea;
+  countrySelect1.appendChild(option);
+}
+
